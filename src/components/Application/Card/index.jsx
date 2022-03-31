1111ab8656a3561de964ef835af96adfe685688a
Giankaz <91921192+Giankaz@users.noreply.github.com>
2022-03-31 15:03:17 -0300

    feat:project finish

--- src/components/Application/Card/index.jsx
+++ src/components/Application/Card/index.jsx
@@ -1,19 +1,12 @@
 import trash from './trash.svg'
 import './style.css'
 
-export default function Card () {
+export default function Card (list) {
 
 
     return (
-        <li key="" className="card">
-            <div className="info">
-                    <p className="infoDes">Salário-Mês Dezembro</p>
-                    <span className="infoType">Entrada</span>
-            </div>
-            <div className="totalDel">
-                    <p>R$ <span className="infoValue">6.660,00</span></p>
-                    <button><img src={trash} alt="" /></button>
-            </div>
-       </li>
+        <>
+       
+        </>
     )
 }
--- src/components/Application/Form/index.jsx
+++ src/components/Application/Form/index.jsx
@@ -1,28 +1,50 @@
 import './style.css'
+import {useState} from 'react'
 
-export default function Form () {
+
+
+export default function Form ({setHave, setList, list, setTotal, total, setCopy}) {
+
+    const [description, setDescription] = useState('')
+    const [value, setValue] = useState('')
+    const [type, setType] = useState('Entrada')
+    const [counter, setCounter] = useState(0)
+
+
+    const handleList = () => {
+        setList([...list, {description: description, value: value, type: type, id: counter}])
+        setCopy([...list, {description: description, value: value, type: type, id: counter}])
+
+        type === 'Entrada' ? setTotal(total + Number(value)) : setTotal(total - Number(value))
+        setCounter(counter + 1)
+        setDescription('')
+        setValue('')
+    }
 
     return (
-        <form className="homeForm">
+        <form className="homeForm" onSubmit={(event) => event.preventDefault()}>
                 <h3>Descrição</h3>
-                <input type="text" placeholder="Digite aqui sua descrição"/>
+                <input type="text" placeholder="Digite aqui sua descrição" onChange={(event) => setDescription(event.target.value)} value={description}/>
                 <span>Ex. Compra de roupas</span>
          <div className="holder">
                 <div className="valor">
                         <h3>Valor</h3>
-                        <input type="number" className="inputValor" placeholder="1"/>
+                        <input type="number" className="inputValor" placeholder="1" onChange={(event) => setValue(event.target.value)} value={value}/>
                         <label htmlFor="inputValor">R$</label>
                 </div>
                 <div className="tipo">
                     <h3>Tipo de valor</h3>
-                    <select>
-                        <option value="entrada" className="entrada">Entrada</option>
-                        <option value="saida" className="saida">Despesa</option>
+                    <select onChange={(event) => setType(event.target.value)} value={type} defaultValue="Entrada">
+                        <option value="Entrada" className="entrada">Entrada</option>
+                        <option value="Despesa" className="saida">Despesa</option>
 
                     </select>
                 </div>
           </div>
-          <button type="submit" className="formSubmit">Inserir Valor</button>
+          <button type="submit" className="formSubmit" onClick={() => {
+              handleList()
+              setHave(true)
+              }}>Inserir Valor</button>
         </form>
     )
 }
